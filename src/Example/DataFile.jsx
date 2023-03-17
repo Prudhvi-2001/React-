02e6498939f3bd4e8798791d50c870bfae6c7f34
prudhvi <kuntrapakamprudhvi@gmail.com>
2023-03-17 23:05:06 +0530

    Data import from other file

---
+++ src/Example/DataFile.jsx
@@ -0,0 +1,27 @@
+const dataFile=[{
+    id:1,
+    name:'prudhvi',
+    age:21,
+    designataion:'B.Tech'
+
+    }, 
+     {
+    id:2,
+    name:'Dileep',
+    age:19,
+    designataion:'M.Tech'
+    },  
+    {
+    id:3,
+    name:'Sekhar',
+    age:45,
+    designataion:'Phd'
+    },
+    {
+        id:1,
+        name:'Mani',
+        age:35,
+        designataion:'Degree'
+    }
+]
+export default dataFile;
--- src/Example/RefForm.jsx
+++ src/Example/RefForm.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import Input from './Input'
 import './ref.css'
+import  dataFile from './DataFile'
 function RefForm() {
     const [values,setValues]=useState({
         fullname:'',
@@ -40,6 +41,18 @@ function RefForm() {
        {userData.map((data)=>(<input {...data} key={data.id} value={values[data.name]} onChange={onChangeSubmit}/>))}
       <input type="submit" value='Submit'/>
       </form>
+      {dataFile.map((sampleData)=>(
+      <>
+      <ul>
+        <li>{sampleData.id}</li>
+        <li>{sampleData.name}</li>
+        <li>{sampleData.age}</li>
+        <li>{sampleData.designataion}</li>
+      </ul>
+      
+      </>
+
+      ))}
     </div>
   )
 }
